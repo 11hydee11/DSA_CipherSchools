4625a30b7f8ae14f3d123cfcbd2795f96e8dcb34
11hydee11 <mohdansari2507@gmail.com>
2023-06-09 07:27:52 +0530

    Day2

---
+++ Pattern.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i<=3; i++) {
+        for(int j = 1; j<=2; i++)
+        cout <<i << " " << j;
+    }
+
+}
