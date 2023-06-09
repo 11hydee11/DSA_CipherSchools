ec70c7282e871a56c880392f2f430b29c612b8f6
11hydee11 <mohdansari2507@gmail.com>
2023-06-09 08:24:47 +0530

    Functions

---
+++ C++ Class work/Functions.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+//Creating and defining the function
+void myFunc() {
+    cout<< "retarded";
+}
+
+int main() {
+  //Calling Function
+  myFunc();
+}
+
